3d8034fa677be45a48adbde67011bdbde1f6b56e
Frugan <dev@frugan.it>
2021-01-13 18:03:04 +0100

    Added command option in pure-ftpd container

--- deploy.sh
+++ deploy.sh
@@ -2,29 +2,32 @@
 
 #https://blog.dockbit.com/templating-your-dockerfile-like-a-boss-2a84a67d28e9
 
-render() {
-  sedStr="
+deploy() {
+  str="
   s!%%TAG%%!$TAG!g;
 "
 
-  sed -r "$sedStr" $1
+  sed -r "$str" $1
 }
 
 TAGS=(
   7.3.13
   7.3.13-prod
 )
+
 ENTRYPOINT=app-entrypoint.sh
 
 for TAG in ${TAGS[*]}; do
+
   if [ -d "$TAG" ]; then
     rm -Rf $TAG
   fi
 
   mkdir $TAG
-  render Dockerfile.template > $TAG/Dockerfile
+  deploy Dockerfile.template > $TAG/Dockerfile
 
   if [ -f "$ENTRYPOINT" ]; then
     cp $ENTRYPOINT $TAG
   fi
+
 done
