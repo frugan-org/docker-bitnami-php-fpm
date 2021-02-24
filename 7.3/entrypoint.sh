ff4fe6c6e126e2b7505de0d38664f071f91ade38
Frugan <dev@frugan.it>
2021-02-24 17:42:45 +0100

    Added locales

--- 7.3/entrypoint.sh
+++ 7.3/entrypoint.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
-set -e
+#!/bin/bash
+
+# shellcheck disable=SC1091
+
+set -o errexit
+set -o nounset
+set -o pipefail
+# set -o xtrace # Uncomment this line for debugging purpose
 
 #https://jtreminio.com/blog/running-docker-containers-as-current-host-user/#ok-so-what-actually-works
 if [ ${USER_ID} -ne 0 ] && [ ${GROUP_ID} -ne 0 ]; then
@@ -17,6 +23,45 @@ if [ ${USER_ID} -ne 0 ] && [ ${GROUP_ID} -ne 0 ]; then
   #    /var/lib/php/sessions
   #;
 fi
+
+
+#https://github.com/bitnami/bitnami-docker-moodle/pull/123
+#https://github.com/bitnami/bitnami-docker-moodle/blob/master/3/debian-10/rootfs/opt/bitnami/scripts/locales/add-extra-locales.sh
+#https://github.com/bitnami/bitnami-docker-moodle#installing-additional-language-packs
+# Defaults
+WITH_ALL_LOCALES="${PHP_WITH_ALL_LOCALES:-no}"
+EXTRA_LOCALES="${PHP_EXTRA_LOCALES:-}"
+
+# Constants
+LOCALES_FILE="/etc/locale.gen"
+SUPPORTED_LOCALES_FILE="/usr/share/i18n/SUPPORTED"
+
+# Helper function for enabling locale only when it was not added before
+enable_locale() {
+    local -r locale="${1:?missing locale}"
+    if ! grep -q -E "^${locale}$" "$SUPPORTED_LOCALES_FILE"; then
+        echo "Locale ${locale} is not supported in this system"
+        return 1
+    fi
+    if ! grep -q -E "^${locale}" "$LOCALES_FILE"; then
+        echo "$locale" >> "$LOCALES_FILE"
+    else
+        echo "Locale ${locale} is already enabled"
+    fi
+}
+
+if [[ "$WITH_ALL_LOCALES" =~ ^(yes|true|1)$ ]]; then
+    echo "Enabling all locales"
+    cp "$SUPPORTED_LOCALES_FILE" "$LOCALES_FILE"
+else
+    LOCALES_TO_ADD="$(sed 's/[,;]\s*/\n/g' <<< "$EXTRA_LOCALES")"
+    while [[ -n "$LOCALES_TO_ADD" ]] && read -r locale; do
+        echo "Enabling locale ${locale}"
+        enable_locale "$locale"
+    done <<< "$LOCALES_TO_ADD"
+fi
+
+locale-gen
 
 
 #https://medium.com/@tomahock/passing-system-environment-variables-to-php-fpm-when-using-nginx-a70045370fad
